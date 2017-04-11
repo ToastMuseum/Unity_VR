8f96d2c9ec732d7f7c67dd5555a9afc394ced15c
toastmuseum <Deolijus@gmail.com>
2017-04-11 16:13:46 -0400

    Created BallSpawner and Camera Rotation
    Camera Rotation: Multiplying Quaternions wil add the component angles.
    
    transform.rotation *= Quaternion.Euler (0, 1, 0);
    
    this will consistently add 1 degree to the y axis rotation and rotate
    the camera 1 degree about the y axis every frame

---
+++ Basket Shooter/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float speed = 5.0f;
+		InstantiateBall (KeyCode.RightArrow, Vector3.right*speed);
+		InstantiateBall (KeyCode.LeftArrow, Vector3.left*speed);
+		InstantiateBall (KeyCode.UpArrow, Vector3.up*speed);
+		InstantiateBall (KeyCode.DownArrow, Vector3.down*speed);
+		
+	}
+
+	void InstantiateBall (KeyCode key, Vector3 initialVelocity){
+		if (Input.GetKeyDown (key)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = initialVelocity;		
+		}
+	}
+
+}
+
---
+++ Basket Shooter/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//transform.rotation = Quaternion.Euler (0, 90, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationSpeed = 5.0f;
+		float mouseX = Input.GetAxis ("Mouse X")*rotationSpeed;
+		float mouseY = Input.GetAxis ("Mouse Y")*rotationSpeed;
+		// multiplying two quaternions results in an addition of angles
+		transform.rotation *= Quaternion.Euler (-mouseY, mouseX, 0);
+	}
+}
