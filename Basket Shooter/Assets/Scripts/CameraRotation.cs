91220f065bb2045b72696061ca4aac9035600bb6
toastmuseum <Deolijus@gmail.com>
2017-04-11 16:59:20 -0400

    Setting up a gimble to correct camera rotation
    By creating a game object for the body the camera's rotation can be
    separated into two steps.
    1. rotate the body horizontally with the camera
    2. rotate just the camera  when moving vertically

--- Basket Shooter/Assets/Scripts/CameraRotation.cs
+++ Basket Shooter/Assets/Scripts/CameraRotation.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour {
 
+
+
 	// Use this for initialization
 	void Start () {
 		//transform.rotation = Quaternion.Euler (0, 90, 0);
+
+
 	}
 	
 	// Update is called once per frame
@@ -15,6 +19,9 @@ public class CameraRotation : MonoBehaviour {
 		float mouseX = Input.GetAxis ("Mouse X")*rotationSpeed;
 		float mouseY = Input.GetAxis ("Mouse Y")*rotationSpeed;
 		// multiplying two quaternions results in an addition of angles
-		transform.rotation *= Quaternion.Euler (-mouseY, mouseX, 0);
+		transform.rotation *= Quaternion.Euler (0, mouseX, 0);
+
+		Camera camera  = GetComponentInChildren<Camera> ();
+		camera.transform.localRotation *= Quaternion.Euler (-mouseY, 0, 0);
 	}
 }
