fb5c8f95907f9cb60758fd2aacd5572b63d1c4ab
toastmuseum <Deolijus@gmail.com>
2017-04-12 01:52:14 -0400

    Allow for changing of scenes in game
    Changing the scene through scripts and build settings index

---
+++ Basket Shooter/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LoadNextScene (KeyCode.Space);
+	}
+
+	void LoadNextScene(KeyCode key){
+		if(Input.GetKeyDown(key)){
+			//Load Scene
+			int currentIndex = SceneManager.GetActiveScene().buildIndex;
+			//load current index + 1
+			SceneManager.LoadScene(currentIndex+1);
+		}
+	}
+}
