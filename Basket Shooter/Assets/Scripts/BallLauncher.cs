c37361abd7fd5b381176c335afae4c200859a5ce
toastmuseum <Deolijus@gmail.com>
2017-04-11 23:17:33 -0400

    Refactor Scripts and Names
    -Replaced BallSpawner script with BallLauncher
    -Refactored functionality to work with the Fire1 button
    -expose ball speed to the inspector

--- Basket Shooter/Assets/Scripts/BallLauncher.cs
+++ Basket Shooter/Assets/Scripts/BallLauncher.cs
@@ -2,32 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallSpawner : MonoBehaviour {
+public class BallLauncher : MonoBehaviour {
 
 	public GameObject ballPrefab;
+	public float ballSpeed = 5.0f;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		float speed = 5.0f;
 		//InstantiateBall (KeyCode.RightArrow, Vector3.right*speed);
 		//InstantiateBall (KeyCode.LeftArrow, Vector3.left*speed);
 		//InstantiateBall (KeyCode.UpArrow, Vector3.up*speed);
 		//InstantiateBall (KeyCode.DownArrow, Vector3.down*speed);
-		InstantiateBall (KeyCode.UpArrow,"Fire1", Vector3.forward*speed);
+		InstantiateBall ("Fire1", Vector3.forward*ballSpeed);
 	}
 
-	void InstantiateBall (KeyCode key, string button, Vector3 initialVelocity){
-		if (Input.GetKeyDown (key)) {
-			GameObject instance = Instantiate (ballPrefab);
-			instance.transform.position = transform.position;
-			Rigidbody rb = instance.GetComponent<Rigidbody> ();
-			rb.velocity = initialVelocity;		
-		}
+	void InstantiateBall (string button, Vector3 initialVelocity){
+
 		if (Input.GetButtonDown ("Fire1")) {
 			GameObject instance = Instantiate (ballPrefab);
 			instance.transform.position = transform.position;
@@ -37,4 +32,3 @@ public class BallSpawner : MonoBehaviour {
 	}
 
 }
-
