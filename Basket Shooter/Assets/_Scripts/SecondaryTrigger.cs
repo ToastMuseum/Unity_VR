28059174aec777dc67f5a11c07ed8e18cabf861b
toastmuseum <Deolijus@gmail.com>
2017-04-14 09:19:55 -0400

    Import Game Assets added triggers for scoring
    Imported Game Assets
    Added to triggers to check if a basket made it in. First trigger
    provides information to second trigger to increment score if basketball
    entered correctly

---
+++ Basket Shooter/Assets/_Scripts/SecondaryTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondaryTrigger : MonoBehaviour {
+
+	Collider expectedCollider;
+	public int targetWorth = 1;
+
+	void OnTriggerEnter(Collider otherCollider){
+		
+		if(otherCollider == expectedCollider){
+			ScoreKeeper scorekeeper = FindObjectOfType<ScoreKeeper> ();
+			scorekeeper.IncrementScore (targetWorth);
+		}
+	}
+
+	public void ExpectCollider(Collider collider){
+		expectedCollider = collider;
+	}
+}
