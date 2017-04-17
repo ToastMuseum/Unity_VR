813cf6a95c949af9f8b4c1ff9589353c0a70ae27
toastmuseum <Deolijus@gmail.com>
2017-04-17 13:05:09 -0400

    Add charging shot functionality
    Chargeshot script that plays a grunt noise when ball is shot and allows
    user to hold down mouse to add more velocity to the shot

---
+++ Basket Shooter/Assets/_Scripts/ChargeShot.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeShot : MonoBehaviour {
+	public GameObject ballPrefab;
+	public float ballSpeed = 5.0f;
+
+	public float maxLaunchSpeed;
+	//public AudioClip chargeShot;
+	public AudioClip launchSound;
+
+	private float speedIncreasePerFrame;
+	[SerializeField]
+	private float launchSpeed;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.clip = launchSound;
+
+		speedIncreasePerFrame = (maxLaunchSpeed * Time.fixedDeltaTime) / 2;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			ChargingShot ();
+		}
+
+		if(Input.GetMouseButtonUp(0)){	
+			audioSource.Stop ();
+			audioSource.clip = launchSound;
+			audioSource.Play ();
+
+			InstantiateBall (Vector3.forward*launchSpeed);
+
+			launchSpeed = 10;
+		}
+		
+	}
+
+	void InstantiateBall (Vector3 initialVelocity){
+		GameObject instance = Instantiate (ballPrefab);
+		instance.transform.position = transform.position;
+
+		//group ball instances into an empty game object
+		instance.transform.parent = GameObject.Find ("Launched Balls").transform;
+
+		Camera camera = GetComponentInChildren<Camera> ();
+
+		Rigidbody rb = instance.GetComponent<Rigidbody> ();
+		rb.velocity = camera.transform.rotation * initialVelocity;
+	}
+
+	void ChargingShot(){
+		if (launchSpeed <= maxLaunchSpeed) {
+			launchSpeed += speedIncreasePerFrame; 
+		}
+	}
+
+
+}
